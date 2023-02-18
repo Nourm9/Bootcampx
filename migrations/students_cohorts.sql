cde0973a9972260cea2c00c3ec024fb500d3c96d
Nour Mohamoud <nour.mohamoud9@gmail.com>
2023-02-18 01:31:37 +0000

    add migrations and seeds for assignment and assignment_submissions

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -1,4 +1,8 @@
-SELECT id, name
-FROM students
-WHERE cohort_id = 1;
-ORDER BY name;
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY NOT null,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+  );
